4dc3554c4c0249ee6662c7a755db2176a86bea88
Lassiter Gregg <lassitergregg@gmail.com>
2018-04-13 15:43:18 -0500

    can send msg and get msgs

--- lib/kele.rb
+++ lib/kele.rb
@@ -14,15 +14,29 @@ class Kele
       puts response.code
     end
     def get_me
-      response = self.class.get(api_endpoint("users/me"), headers: {"content_type" => 'application/json', "authorization" => @auth_token })
+      response = self.class.get(api_endpoint("users/me"), headers: {"authorization" => @auth_token })
       puts response.code
       puts @user = JSON.parse(response.body)
     end
     def get_mentor_availability
       mentor_id = @user['current_enrollment']['mentor_id']
-      response = self.class.get(api_endpoint("mentors/#{mentor_id}/student_availability"), headers: {"content_type" => 'application/json', "authorization" => @auth_token })
+      response = self.class.get(api_endpoint("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token })
       puts response.code
       puts @mentor_availablity = JSON.parse(response.body)
+    end
+    def get_messages(page = nil)
+      if page == nil
+        response = self.class.get(api_endpoint("message_threads"), headers: { "authorization" => @auth_token})
+      else
+        response = self.class.get(api_endpoint("message_threads?page=#{page}"), headers: { "authorization" => @auth_token })
+      end
+      puts response.code
+      puts @messages = JSON.parse(response.body)
+    end
+    def create_message(recipient_id, subject, stripped_text)
+      self.get_me if self.user == nil
+      response = self.class.post(api_endpoint("messages"), body: { "sender": self.user["email"], "recipient_id": recipient_id, "subject": subject, "stripped-text": stripped_text }, headers: {"authorization" => @auth_token })
+      puts response.code
     end
   private
     def api_endpoint(endpoint)
