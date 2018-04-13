551affc0d07c779218b8a7aad5db8a6a50dce954
Lassiter Gregg <lassitergregg@gmail.com>
2018-04-12 22:39:41 -0500

    added get_me

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'httparty', '~> 0.13'
+gem 'json', '~> 1.8', '>= 1.8.3'
--- lib/kele.rb
+++ lib/kele.rb
@@ -1,14 +1,20 @@
 require 'httparty'
+require 'json'
 
 class Kele
+  include JSON
   include HTTParty
     def initialize(email, password)
       response = self.class.post(api_endpoint("sessions"), body: { "email": email, "password": password })
       raise "Email or password was incorrect" if response.code === 401
-      auth_token = response["auth_token"]
+      @auth_token = response["auth_token"]
       puts response.code
     end
-
+    def get_me
+      response = self.class.get(api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+      puts response.code
+      puts @user = JSON.parse(response.body)
+    end
   private
     def api_endpoint(endpoint)
       "https://www.bloc.io/api/v1/#{endpoint}"
