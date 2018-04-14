9a93e44405ace17f411b0f77c3d03383bda747b8
Lassiter Gregg <lassitergregg@gmail.com>
2018-04-14 15:42:40 -0500

    removed instance variables and changed methods to require arguments each time and added token argument

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,18 +11,15 @@ class Kele
       response = self.class.post(api_endpoint("sessions"), body: { "email": email, "password": password })
       raise "Email or password was incorrect" if response.code === 401
       @auth_token = response["auth_token"]
-      puts response.code
     end
     def get_me
       response = self.class.get(api_endpoint("users/me"), headers: {"authorization" => @auth_token })
-      puts response.code
-      puts @user = JSON.parse(response.body)
+      user = JSON.parse(response.body)
     end
     def get_mentor_availability
       mentor_id = @user['current_enrollment']['mentor_id']
       response = self.class.get(api_endpoint("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token })
-      puts response.code
-      puts @mentor_availablity = JSON.parse(response.body)
+      mentor_availablity = JSON.parse(response.body)
     end
     def get_messages(page = nil)
       if page == nil
@@ -30,13 +27,10 @@ class Kele
       else
         response = self.class.get(api_endpoint("message_threads?page=#{page}"), headers: { "authorization" => @auth_token })
       end
-      puts response.code
-      puts @messages = JSON.parse(response.body)
+      messages = JSON.parse(response.body)
     end
-    def create_message(recipient_id, subject, stripped_text)
-      self.get_me if self.user == nil
-      response = self.class.post(api_endpoint("messages"), body: { "sender": self.user["email"], "recipient_id": recipient_id, "subject": subject, "stripped-text": stripped_text }, headers: {"authorization" => @auth_token })
-      puts response.code
+    def create_message(user_email, recipient_id, subject, token, stripped_text)
+      response = self.class.post(api_endpoint("messages"), body: { "sender": user_email, "recipient_id": recipient_id, "subject": subject, "token": token, "stripped-text": stripped_text }, headers: {"authorization" => @auth_token })
     end
   private
     def api_endpoint(endpoint)
